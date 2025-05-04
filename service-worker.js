5a302a4ff3e01c43f430d8b53369c28e8a54af76
Micfri <44413144+mikefri@users.noreply.github.com>
2025-05-04 14:15:24 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,22 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("radios-cache").then(cache => {
+      return cache.addAll([
+        "/RadiosWeb/index.html",
+        "/RadiosWeb/manifest.json",
+        "/RadiosWeb/icons/icon-192.png",
+        "/RadiosWeb/icons/icon-512.png",
+        // ajoute ici d'autres ressources importantes
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
