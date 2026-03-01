8eddce3569ab4777d618c75b3b1e310e65a88e19
Micfri <44413144+mikefri@users.noreply.github.com>
2026-03-01 17:47:15 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("radiosweb-v1").then(cache => {
+    caches.open("radiosweb-v2").then(cache => {
       return cache.addAll([
         "/RadiosWeb/index.html",
         "/RadiosWeb/manifest.json",
